cce572edb9275116aeac96d0a7fd8973702c02de
Ksawery <ksawery9708@gmail.com>
2024-09-15 12:08:03 +0200

    feat: added autoSubmit functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+
+function autoSubmit() {
+    const callback = (mutations) => {
+        mutations.forEach(mutation => {
+            const element = mutation.target;
+            // contains class ng-dirty, means that form has value selected
+            if (element.classList.contains('ng-dirty'))
+            {
+                // click start if it's not clicked
+                document.getElementById('button-start')?.click();
+                // submit
+                document.getElementById('button-next')?.click();
+            }
+        })
+    };
+    const answerForm = document.querySelector('.answer form');
+    const observer = new MutationObserver(callback)
+    observer.observe(answerForm, {
+        attributes: true, 
+        attributeFilter: ['class'] 
+    });
+}
+
 // test started
 document.getElementById('start-test').addEventListener('click', () => {
     // wait for media to render
@@ -5,6 +28,9 @@ document.getElementById('start-test').addEventListener('click', () => {
         const mediaContainer = document.querySelector('#test-container section.media');
         if (!mediaContainer)
             return;
+
+        // add auto submit on answer select
+        autoSubmit();
         // add event to show video on media container click 
         mediaContainer.addEventListener('click', () => {
             document.getElementById('button-start')?.click();
