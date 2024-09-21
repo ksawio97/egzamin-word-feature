aed65260898bd73f0098530de90cc5576ce4e657
Ksawery <ksawery9708@gmail.com>
2024-09-21 19:50:57 +0200

    feat: added reading questions

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function readAloud() {
+    const callback = (mutations) => {
+        mutations.forEach((mutation) => {
+            if (mutation.type === 'characterData') {
+                const text = mutation.target.textContent;
+                // Add your custom logic here
+                window.speechSynthesis.cancel();
+                const utter = new SpeechSynthesisUtterance(text);
+                utter.lang = 'pl';
+                window.speechSynthesis.speak(utter);
+            }
+          });
+    };
+
+    const questionContent = document.getElementById('question-content');
+
+    const observerConfig = {
+        characterData: true,
+        characterDataOldValue: true,
+        subtree: true
+    };
+    const observer = new MutationObserver(callback);
+    observer.observe(questionContent, observerConfig);
+}
 
 function autoSubmit() {
     const callback = (mutations) => {
@@ -20,22 +44,28 @@ function autoSubmit() {
         attributeFilter: ['class'] 
     });
 }
+const interval = setInterval(() => {
+    const testStart = document.getElementById('start-test');
+    if (!testStart)
+        return;
+    clearInterval(interval);
+    // test started
+    testStart.addEventListener('click', () => {
+        // wait for media to render
+        const interval = setInterval(() => {
+            const mediaContainer = document.querySelector('#test-container section.media');
+            if (!mediaContainer)
+                return;
 
-// test started
-document.getElementById('start-test').addEventListener('click', () => {
-    // wait for media to render
-    const interval = setInterval(() => {
-        const mediaContainer = document.querySelector('#test-container section.media');
-        if (!mediaContainer)
-            return;
+            readAloud();
+            // add auto submit on answer select
+            autoSubmit();
+            // add event to show video on media container click 
+            mediaContainer.addEventListener('click', () => {
+                document.getElementById('button-start')?.click();
+            });
 
-        // add auto submit on answer select
-        autoSubmit();
-        // add event to show video on media container click 
-        mediaContainer.addEventListener('click', () => {
-            document.getElementById('button-start')?.click();
-        });
-
-        clearInterval(interval);
-    }, 100);
-});
+            clearInterval(interval);
+        }, 100);
+    });
+}, 100);
